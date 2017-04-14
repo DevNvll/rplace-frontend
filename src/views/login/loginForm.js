ad7707a56bb94a0e17babe027d112620128da6e3
Henrick Mello <hmartins505@gmail.com>
2017-04-14 16:33:55 -0300

    Added register validation and error alerts.

--- src/views/login/loginForm.js
+++ src/views/login/loginForm.js
@@ -4,21 +4,26 @@ import {
   Form, 
   FormGroup, 
   Label, 
-  Input 
+  Input,
+  Alert
 } from 'reactstrap'
 import { Link, Redirect } from 'react-router-dom'
 import Recaptcha from 'react-google-recaptcha'
 
 import { login, checkAuth } from '../../utils/auth'
-
+import STATUS_CODES from '../../utils/statusCodes'
+import { CAPTCHA_SITE_KEY } from '../../config'
 
 export default class LoginForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      passwordError: false,
+      errorVisible: false,
       recaptcha: null
     }
+  }
+  onDismiss() {
+    this.setState({errorVisible: false})
   }
   onCaptcha(code) {
     this.setState({recaptcha: code})
@@ -27,10 +32,10 @@ export default class LoginForm extends Component {
     this.forceUpdate()
   }
   onLoginError(err) {
-    this.setState({passwordError: true})
+    this.setState({errorVisible: true, errcode: STATUS_CODES[err]})
   }
   onSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
     login(e.target.email.value, e.target.password.value, this.state.recaptcha, this.onLogin.bind(this), this.onLoginError.bind(this))
   }
   render() {
@@ -42,7 +47,7 @@ export default class LoginForm extends Component {
     return(
       <div>
         <Form onSubmit={this.onSubmit.bind(this)}>
-          {(this.state.passwordError && (<h5>Wrong email/password</h5>))}
+          <Alert color="danger" isOpen={this.state.errorVisible} toggle={this.onDismiss.bind(this)}>{this.state.errcode}</Alert>
           <FormGroup>
             <Label for="email">E-mail</Label>
             <Input type="text" name="email" id="email" placeholder="your email" />
@@ -51,7 +56,7 @@ export default class LoginForm extends Component {
             <Label for="password">Password</Label>
             <Input type="password" name="password" id="password" placeholder="your password" />
           </FormGroup>
-          <Recaptcha ref="recaptcha" sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" onChange={this.onCaptcha.bind(this)} />
+          <Recaptcha ref="recaptcha" sitekey={CAPTCHA_SITE_KEY} onChange={this.onCaptcha.bind(this)} />
           <Button color="primary">Login</Button> <Link to="/register">Create an account</Link>
         </Form>
       </div>
--- src/views/register/registerForm.js
+++ src/views/register/registerForm.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react'
-import { Button, 
-  Form, 
-  FormGroup, 
-  Label, 
-  Input 
+import { 
+  Button,
+  Label,
+  Alert
 } from 'reactstrap'
 import { Link, Redirect } from 'react-router-dom'
+import { AvForm as Form, AvField as Input, AvGroup as FormGroup, AvFeedback } from 'availity-reactstrap-validation';
 
 import {checkAuth, register} from '../../utils/auth'
+import STATUS_CODES from '../../utils/statusCodes'
+
 import Success from './successMessage'
 
 export default class RegisterForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      registered: false
+      registered: false,
+      errorVisible: false
     }
+  }
+  onDismiss() {
+    this.setState({errorVisible: false})
   }
   onRegister() {
     this.setState({registered: true})
   }
-  onSubmit(e) {
-    e.preventDefault()
-    if(e.target.password.value === e.target.passwordConfirm.value)
-      register(e.target.username.value, e.target.email.value, e.target.password.value, this.onRegister.bind(this))
-    else
-      console.log("password didn't match")
+  onError(err) {
+    this.setState({errorVisible: true, errcode: STATUS_CODES[err]})
+  }
+  onSubmit(e, values) {
+    register(values.username, values.email, values.password, this.onRegister.bind(this), this.onError.bind(this))
   }
   render() {
     if (checkAuth()) {
@@ -38,22 +43,26 @@ export default class RegisterForm extends Component {
     }
     return(
       <div>
-        <Form onSubmit={this.onSubmit.bind(this)}>
+        <Form onValidSubmit={this.onSubmit.bind(this)}>
+          <Alert color="danger" isOpen={this.state.errorVisible} toggle={this.onDismiss.bind(this)}>{this.state.errcode}</Alert>
           <FormGroup>
             <Label for="username">Username</Label>
-            <Input type="text" name="username" id="username" placeholder="your username" />
+            <Input type="text" name="username" id="username" placeholder="your username" minLength={5} required/>
+            <AvFeedback>Username must have at least 5 characters</AvFeedback>
           </FormGroup>
           <FormGroup>
-            <Label for="email">Email</Label>
-            <Input type="email" name="email" id="email" placeholder="example@gmail.com" />
+            <Label for="email">E-mail</Label>
+            <Input type="email" name="email" id="email" placeholder="example@gmail.com" required/>
           </FormGroup>
           <FormGroup>
             <Label for="password">Password</Label>
-            <Input type="password" name="password" id="password" placeholder="your password" />
+            <Input type="password" name="password" id="password" placeholder="your password" minLength={6} required/>
+            <AvFeedback>Passwords must have at least 6 characters</AvFeedback>
           </FormGroup>
           <FormGroup>
             <Label for="password">Confirm password</Label>
-            <Input type="password" name="passwordConfirm" id="passwordConfirm" placeholder="confirm your password" />
+            <Input type="password" name="passwordConfirm" id="passwordConfirm" placeholder="confirm your password" validate={{match:{value:'password'}}} required/>
+            <AvFeedback>Passwords must match</AvFeedback>
           </FormGroup>
           <Button color="primary">Register</Button> <Link to="/login">Login to your account</Link>
         </Form>
